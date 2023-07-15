5b169ddf7ed00434b793ca122c9198228dcfff14
nholguinj <135862466+nholguinj@users.noreply.github.com>
2023-07-15 14:52:05 -0500

    objectscript.js
    Se agrega la variable Pedido

--- objectscript.js
+++ objectscript.js
@@ -1,19 +1,28 @@
+let numero = localStorage.getItem('numeroList');
 let items = localStorage.getItem('itemList');
 let cant = localStorage.getItem('cantList');
 let precio = localStorage.getItem('precioList');
 
+
+numero = numero ? JSON.parse(numero) : [];
 items = items ? JSON.parse(items) : [];
 cant = cant ? JSON.parse(cant) : [];
 precio = precio ? JSON.parse(precio) : [];
 
+
+
 showItem();
 
 function addItem() {
+  let pedido = document.getElementById('pedido').value;
   let producto = document.getElementById('producto').value;
   let cantidad = document.getElementById('cantidad').value;
   let valor = document.getElementById('valor').value;
 
-  if (producto && cantidad && valor) {
+
+
+  if (pedido && producto && cantidad && valor) {
+    numero.push(pedido);
     items.push(producto);
     cant.push(cantidad);
     precio.push(valor);
@@ -24,9 +33,11 @@ function addItem() {
 }
 
 function showItem() {
-  document.getElementById('producto').value = '';
+  document.getElementById('pedido').value = '';
+  document.getElementById('producto').value =  '';
   document.getElementById('cantidad').value = '';
   document.getElementById('valor').value = '';
+
 
   let html = '';
 
@@ -37,11 +48,12 @@ function showItem() {
       let total = cant[i] * precio[i]; // Calcular el total de precio por cantidad
 
       html += `<div class="row">
+                  <div class="col-1 mb-3">${numero[i]}</div>
                   <div class="col-1 mb-3">${cant[i]}</div>
                   <div class="col-4 mb-3">${items[i]}</div>
-                  <div class="col-3 mb-3">${precio[i]}</div>
-                  <div class="col-3 mb-3">${total}</div>
-                  <div class="col-1 mb-3">
+                  <div class="col-2 mb-3">${precio[i]}</div>
+                  <div class="col-2 mb-3">${total}</div>
+                  <div class="col-2 mb-3">
                     <a href="#" class="btn btn-danger" onclick="deleteItem(${i})">x</a>
                   </div>
                 </div>`;
@@ -52,15 +64,18 @@ function showItem() {
 }
 
 function deleteItem(index) {
+  numero.splice(index, 1);
   items.splice(index, 1);
   cant.splice(index, 1);
   precio.splice(index, 1);
+
 
   saveToLocalStorage();
   showItem();
 }
 
 function saveToLocalStorage() {
+  localStorage.setItem('numeroList', JSON.stringify(numero));
   localStorage.setItem('itemList', JSON.stringify(items));
   localStorage.setItem('cantList', JSON.stringify(cant));
   localStorage.setItem('precioList', JSON.stringify(precio));
